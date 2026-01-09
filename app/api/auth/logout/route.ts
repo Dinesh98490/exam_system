d99c17d7b2f67afdf4e74587c78a3d1f732a5d1a
“Dinesh98490” <kunwardinesh028@gmail.com>
2026-01-09 15:20:21 +0545

    logout logics

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { deleteSession } from "@/lib/auth/session";
 
+
+// logout handler request
 export async function POST(req: Request) {
   await deleteSession();
   return NextResponse.redirect(new URL("/login", req.url));
