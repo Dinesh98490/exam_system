a5614e1c1a85a265e1c38c193a23e88e2e0efd8d
“Dinesh98490” <kunwardinesh028@gmail.com>
2026-01-13 22:55:14 +0545

    exam access

--- models/ExamAccess.ts
+++ models/ExamAccess.ts
@@ -2,6 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 
 // Schema to track which users have access to which exams
+// This includes start time, completion time, score, and answers given
 const ExamAccessSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   examId: { type: Schema.Types.ObjectId, ref: 'Exam', required: true },
